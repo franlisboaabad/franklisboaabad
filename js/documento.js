ddc964ad0d47e3e01f31bd7acac5c678b2491bc8
franklisboaabad <franco_lisboa@hotmail.com>
2020-04-15 10:20:48 -0500

    App vue
    Vue portafolio

--- js/documento.js
+++ js/documento.js
@@ -1,7 +1,11 @@
+
+
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
 });
+
+
 
 // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
 // var collapsibleElem = document.querySelector('.collapsible');
@@ -12,42 +16,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
 $(document).ready(function () {
     $('.sidenav').sidenav();
-});
-
-
-
-$(document).ready(function () {
     $('.fixed-action-btn').floatingActionButton();
-});
-
-var instance = M.Carousel.init({
-    fullWidth: true
-});
-
-// Or with jQuery
-
-$('.carousel.carousel-slider').carousel({
-    fullWidth: true
-});
-
-$(document).ready(function () {
     $('.materialboxed').materialbox();
-});
-
-/*paralax*/
-$(document).ready(function () {
     $('.parallax').parallax();
-});
-
-$(document).ready(function () {
     $('.slider').slider();
 });
-
-
-// Or with jQuery
-
-$(document).ready(function () {
-    $('.carousel').carousel();
-});
-
-/*mimodal*/
