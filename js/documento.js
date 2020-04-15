d723268384c894282924c8150680739d7e5edb2d
franklisboaabad <franco_lisboa@hotmail.com>
2020-04-15 11:37:24 -0500

    App

--- js/documento.js
+++ js/documento.js
@@ -1,11 +1,7 @@
-
-
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
 });
-
-
 
 // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
 // var collapsibleElem = document.querySelector('.collapsible');
@@ -16,8 +12,42 @@ document.addEventListener('DOMContentLoaded', function () {
 
 $(document).ready(function () {
     $('.sidenav').sidenav();
+});
+
+
+
+$(document).ready(function () {
     $('.fixed-action-btn').floatingActionButton();
+});
+
+var instance = M.Carousel.init({
+    fullWidth: true
+});
+
+// Or with jQuery
+
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true
+});
+
+$(document).ready(function () {
     $('.materialboxed').materialbox();
+});
+
+/*paralax*/
+$(document).ready(function () {
     $('.parallax').parallax();
+});
+
+$(document).ready(function () {
     $('.slider').slider();
 });
+
+
+// Or with jQuery
+
+$(document).ready(function () {
+    $('.carousel').carousel();
+});
+
+/*mimodal*/
