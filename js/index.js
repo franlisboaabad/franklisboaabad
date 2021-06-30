1eb743510ba4e760f1a89ea8ce16b95dcb2ece86
franklisboaabad <franco_lisboa@hotmail.com>
2021-06-29 20:16:18 -0500

    Landing
    Nueva lading portafolio gamer

---
+++ js/index.js
@@ -0,0 +1,238 @@
+
+function Proyects() {
+
+    document.getElementById('section_web').innerHTML = `
+        <div class="col-md-4 pb-3">
+        <div class="card">
+            <a class="portfolio-box" href="assets/portafolio/seven.jpg">
+                <img src="assets/portafolio/seven.jpg" class="card-img-top" alt="desarrollo-web">
+            </a>
+            <div class="card-body">
+                <p class="card-title">SEVEN STAR PERÚ  </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4 pb-3">
+        <div class="card">
+            <a class="portfolio-box" href="assets/portafolio/construye.jpg">
+                <img src="assets/portafolio/construye.jpg" class="card-img-top" alt="desarrollo-web">
+            </a>
+            <div class="card-body">
+                <p class="card-title"> ATLAS INGENIERÍA Y CONSTRUCCIÓN </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4 pb-3">
+        <div class="card">
+            <a class="portfolio-box" href="assets/portafolio/alemi.jpg">
+                <img src="assets/portafolio/alemi.jpg" class="card-img-top" alt="desarrollo-web">
+            </a>
+            <div class="card-body">
+                <p class="card-title"> ALEMI DEL NORTE </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4 pb-3">
+        <div class="card">
+            <a class="portfolio-box" href="assets/portafolio/ecoturismo.jpg">
+                <img src="assets/portafolio/ecoturismo.jpg" class="card-img-top" alt="desarrollo-web">
+            </a>
+            <div class="card-body">
+                <p class="card-title"> ECOTURISMO PIURA </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4 pb-3">
+        <div class="card">
+            <a class="portfolio-box" href="assets/portafolio/barberia.jpg">
+                <img src="assets/portafolio/barberia.jpg" class="card-img-top" alt="desarrollo-web">
+            </a>
+
+            <div class="card-body">
+                <p class="card-title"> BARBERSHOP - EL NUEVO ESTILO  </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4 pb-3">
+        <div class="card">
+           
+            <a class="portfolio-box" href="assets/portafolio/ideaspiura.jpg">
+                <img src="assets/portafolio/ideaspiura.jpg" class="card-img-top" alt="desarrollo-web">
+            </a>
+
+            <div class="card-body">
+                <p class="card-title"> IDEASPIURA TOURS  </p>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="col-md-4 pb-3">
+        <div class="card">
+            <a class="portfolio-box" href="assets/portafolio/mancora.jpg">
+                <img src="assets/portafolio/mancora.jpg" class="card-img-top" alt="desarrollo-web">
+            </a>
+
+            <div class="card-body">
+                <p class="card-title"> MANCORA TOURS</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4 pb-3">
+        <div class="card">
+            <a class="portfolio-box" href="assets/portafolio/proyectosayc.jpg">
+                <img src="assets/portafolio/proyectosayc.jpg" class="card-img-top" alt="desarrollo-web">
+            </a>
+
+
+            <div class="card-body">
+                <p class="card-title">PROYETOS AYC</p>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="col-md-4 pb-3">
+        <div class="card">
+            
+            <a class="portfolio-box" href="assets/portafolio/nefasasac.jpg">
+                <img src="assets/portafolio/nefasasac.jpg" class="card-img-top" alt="desarrollo-web">
+            </a>
+
+            <div class="card-body">
+                <p class="card-title"> NEFASA SAC </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4 pb-3">
+        <div class="card">
+            
+            <a class="portfolio-box" href="assets/portafolio/delatiendatucasa.jpg">
+                <img src="assets/portafolio/delatiendatucasa.jpg" class="card-img-top" alt="desarrollo-web">
+            </a>
+
+            <div class="card-body">
+                <p class="card-title"> DELATIENDA A TU CASA </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4 pb-3">
+        <div class="card">
+            
+            <a class="portfolio-box" href="assets/portafolio/dylkao-delivery.jpg">
+                <img src="assets/portafolio/dylkao-delivery.jpg" class="card-img-top" alt="desarrollo-web">
+            </a>
+
+            <div class="card-body">
+                <p class="card-title"> DYLKAO MARKET </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4 pb-3">
+        <div class="card">
+            
+            <a class="portfolio-box" href="assets/portafolio/rapidosdelnorte.jpg">
+                <img src="assets/portafolio/rapidosdelnorte.jpg" class="card-img-top" alt="desarrollo-web">
+            </a>
+
+            <div class="card-body">
+                <p class="card-title"> LOS RAPIDOS DEL NORTE  </p>
+            </div>
+        </div>
+    </div>
+
+        `;
+
+
+};
+
+function Wordpress() {
+
+    document.getElementById('section_worpress').innerHTML =
+        `
+        <div class="col-md-4 pb-3">
+            <div class="card">
+                
+                <a class="portfolio-box" href="assets/portafolio/capofruits.jpg">
+                    <img src="assets/portafolio/capofruits.jpg" class="card-img-top" alt="desarrollo-web">
+                </a>
+
+                <div class="card-body">
+                    <p class="card-title"> CAPO FRUITS  </p>
+                </div>
+            </div>
+        </div>
+
+            <div class="col-md-4 pb-3">
+                <div class="card">
+                    
+                    <a class="portfolio-box" href="assets/portafolio/munivice.jpg">
+                        <img src="assets/portafolio/munivice.jpg" class="card-img-top" alt="desarrollo-web">
+                    </a>
+
+                    <div class="card-body">
+                        <p class="card-title"> MUNICIPALIDAD DISTRITAL DE VICE  </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 pb-3">
+                <div class="card">
+                    
+                    <a class="portfolio-box" href="assets/portafolio/munibernal.jpg">
+                        <img src="assets/portafolio/munibernal.jpg" class="card-img-top" alt="desarrollo-web">
+                    </a>
+
+                    <div class="card-body">
+                        <p class="card-title"> MUNICIPALIDAD DISTRITAL DE BERNAL  </p>
+                    </div>
+                </div>
+            </div>
+
+            
+            <div class="col-md-4 pb-3">
+                <div class="card">
+                    
+                    <a class="portfolio-box" href="assets/portafolio/sjagile.jpg">
+                        <img src="assets/portafolio/sjagile.jpg" class="card-img-top" alt="desarrollo-web">
+                    </a>
+
+                    <div class="card-body">
+                        <p class="card-title"> SJ AGILE CONSULTING  </p>
+                    </div>
+                </div>
+            </div>
+
+        `;
+}
+
+Proyects();
+
+
+
+
+
+document.getElementById('button_paginas').addEventListener('click', function (e) {
+
+    document.getElementById('section_web').style.display = '';
+    document.getElementById('section_wordpress').style.display = 'none';
+})
+
+
+
+document.getElementById('button_wordpress').addEventListener('click', function (e) {
+
+    document.getElementById('section_web').style.display = 'none';
+    Wordpress();
+
+})
+
